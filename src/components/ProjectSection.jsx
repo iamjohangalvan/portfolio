b39b5b27a2fb29a3530cff5be211bc5b5159845e
iamjohangalvan <iamjohangalvan@gmail.com>
2025-08-27 10:42:03 -0400

    updated ProjectSection

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -3,32 +3,32 @@ import { ExternalLink, Github } from "lucide-react";
 const projects = [
   {
     id: 1,
-    title: "SaaS Landing Page",
-    description: "A beautiful landing page app using React and Tailwind.",
+    title: "Portfolio Landing Page",
+    description: "A beautiful landing page portfoliousing React and Tailwind.",
     image: "/projects/project1.png",
-    tags: ["React", "TailwindCSS", "Supabase"],
-    demoUrl: "#",
-    githubUrl: "#",
+    tags: ["React", "TailwindCSS"],
+    demoUrl: "https://www.johangalvan.com/",
+    githubUrl: "https://github.com/iamjohangalvan/portfolio",
   },
   {
     id: 2,
-    title: "Orbit Analytics Dashboard",
+    title: "Calendar App",
     description:
-      "Interactive analytics dashboard with data visualization and filtering capabilities.",
+      "Full-featured MERN calendar app with user authentication and event scheduling.",
     image: "/projects/project2.png",
-    tags: ["TypeScript", "D3.js", "Next.js"],
-    demoUrl: "#",
-    githubUrl: "#",
+    tags: ["React", "Node.js", "MongoDB"],
+    demoUrl: "https://calendar-app-mern-backend.up.railway.app/",
+    githubUrl: "https://github.com/iamjohangalvan/calendar-app",
   },
   {
     id: 3,
     title: "E-commerce Platform",
     description:
-      "Full-featured e-commerce platform with user authentication and payment processing.",
+      "Full-featured MERN e-commerce platform with user authentication and payment processing.",
     image: "/projects/project3.png",
     tags: ["React", "Node.js", "Stripe"],
-    demoUrl: "#",
-    githubUrl: "#",
+    demoUrl: "https://mern-ecommerce-cd2b.onrender.com/",
+    githubUrl: "https://github.com/iamjohangalvan/mern-ecommerce",
   },
 ];
 export const ProjectSection = () => {
