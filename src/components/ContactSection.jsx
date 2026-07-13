94f825998b473ba2de2e233a402f857ae3e9d6a0
Johan Galvan <iamjohangalvan@gmail.com>
2026-07-13 10:59:42 -0400

    updated contact section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -41,7 +41,7 @@ export const ContactSection = () => {
             <div className="space-y-6 justify-center">
               <div className="flex items-center space-x-4">
                 <div className="p-3 rounded-full bg-primary/10">
-                  <Mail className="h-6 w-6 text-primary" />{" "}
+                  {" "}
                 </div>
                 <div>
                   <h4 className="font-medium"> Email</h4>
@@ -55,7 +55,7 @@ export const ContactSection = () => {
               </div>
               <div className="flex items-center space-x-4">
                 <div className="p-3 rounded-full bg-primary/10">
-                  <Phone className="h-6 w-6 text-primary" />{" "}
+                 {" "}
                 </div>
                 <div>
                   <h4 className="font-medium"> Phone</h4>
@@ -69,7 +69,7 @@ export const ContactSection = () => {
               </div>
               <div className="flex items-center space-x-4">
                 <div className="p-3 rounded-full bg-primary/10">
-                  <MapPin className="h-6 w-6 text-primary" />{" "}
+                  {" "}
                 </div>
                 <div>
                   <h4 className="font-medium"> Location</h4>
@@ -100,78 +100,9 @@ export const ContactSection = () => {
               </div>
             </div>
           </div>
-          <div
-            className="bg-card p-8 rounded-lg shadow-xs"
-            onSubmit={handleSubmit}
-          >
-            <h3 className="text-2xl font-semibold mb-6">Send a Message</h3>
-            <form className="space-y-6">
-              <div>
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium mb-2"
-                >
-                  {" "}
-                  Your Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
-                  placeholder="John Doe..."
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium mb-2"
-                >
-                  {" "}
-                  Your Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
-                  placeholder="john@gmail.com..."
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="message"
-                  className="block text-sm font-medium mb-2"
-                >
-                  {" "}
-                  "Hello, I'd like to talk about..."
-                </label>
-                <input
-                  type="textarea"
-                  id="message"
-                  name="message"
-                  required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary resize-none"
-                  placeholder="John Doe..."
-                />
-              </div>
-              <button
-                type="submit"
-                disabled={isSubmitting}
-                className={cn(
-                  "cosmic-button w-full flex items-center justify-center gap-2"
-                )}
-              >
-                {isSubmitting ? "Sending..." : "Send Message"}
-                Send Message
-                <Send size={16} />
-              </button>
-            </form>
+
           </div>
         </div>
-      </div>
-    </section>
+      </section>
   );
 };
